f3fa9746c5285431f951a1f5b5635e85d4c2a099
Orumant <asp.w@inbox.ru>
2019-05-22 18:40:31 +0300

    - выгрузка статистики

--- src/statistics/mobius.js
+++ src/statistics/mobius.js
@@ -14,54 +14,115 @@ const ANDROID_FILE_HEADER =
 const IOS_FILE_HEADER =
   'login,username,Статус,Рейтинг,Количество попыток,block1,block2,block3,block4,block5,' +
   'block6,block7,block8,block9,block10\n';
+const GLOBAL_STAT_HEADER = `Всего;Android;iOS;% успеха Android;% успеха iOS\n`;
+const CATEGORIES_STAT_HEADER = 'Категория;% успеха\n';
 
-module.exports = {
-  usersByTryes,
-};
+mongoose.connect(`mongodb://185.185.40.107:27017/quiz_db`).then(() => {
+  Promise.all([
+    getAllUsers(),
+    getAllCategories(),
+  ])
+    .then(R.zipObj(['users', 'categories']))
+    .then(({users, categories}) => {
+      function getBestAnswer({answers, oldAnswers}) {
+        if (oldAnswers.length === 0) {
+          return answers;
+        } else {
+          return R.concat([answers], oldAnswers).reduce((prev, cur) => {
+            let prevScore = countCorrectAnswers(prev);
+            let curScore = countCorrectAnswers(cur);
+            return prevScore >= curScore ? prev : cur
+          });
+        }
+      }
 
-function isUserSucceed(user) {
-  if (user.status !== FINISH_STATUS) return false;
-  if (user.oldAnswers.length === 0) {
-    let score = countCorrectAnswers(user.answers);
-    return score >= LOTTERY_SCORE;
-  } else {
-    return user.oldAnswers.some(answers => {
-      return countCorrectAnswers(answers) >= LOTTERY_SCORE
-    })
-  }
-}
+      function countScore(user) {
+        let allAnswers = R.concat([user.answers], user.oldAnswers);
+        let scores = allAnswers.map(answers => countCorrectAnswers(answers));
+        let topScore = Math.max(...scores);
+        return {
+          score: `${topScore}/10`,
+          attemptsQuantity: `${allAnswers.length}/${MAX_TRY}`,
+          result: topScore >= LOTTERY_SCORE ? 'SUCCESS' : 'FAILED',
+        };
+      }
 
-function countScore(user) {
-  let allAnswers = R.concat([user.answers], user.oldAnswers);
-  let scores = allAnswers.map(answers => countCorrectAnswers(answers));
-  let topScore = Math.max(...scores);
-  return {
-    score: `${topScore}/10`,
-    attemptsQuantity: `${allAnswers.length}/${MAX_TRY}`,
-    result: topScore >= LOTTERY_SCORE ? 'SUCCESS' : 'FAILED',
-  };
-}
+      function getSucessPercentString(users) {
+        let sucess = [];
+        users.forEach(user => {
+          const {result} = countScore(user);
+          if (result === 'SUCCESS') sucess.push(user);
+        });
+        return `${Math.floor((sucess.length * 100) / users.length).toString()}%`;
+      }
 
-function аndroidUsersStats() {
-  return new Promise((resolve, reject) => {
-    connect().then(() => {
-      Promise.all([
-        getAllUsers(),
-        getAllCategories,
-        getAllQuestionnaires,
-      ])
-        .then(R.zipObj(['users', 'categories', 'questions']))
-        .then(({users, categories, questions}) => {
-          let androidDevs = users.filter(user => user.stack === 'android');
-          let fileBody = '';
-          androidDevs.forEach(user => {
-            const {username, fio} = user;
-            let result = countScore(user);
-            fileBody += `${username},${fio},${result.result},${result.score},${result.attemptsQuantity}`;
+      function fillData(users, fileBody) {
+        users.forEach(user => {
+          const {username, fio} = user;
+          let result = countScore(user);
+          fileBody += `${username},${fio},${result.result},${result.score},${result.attemptsQuantity},`;
+          let bestAnswer = getBestAnswer(user);
+          let curAnswers = bestAnswer.map(answer => `${answer.isCorrect ? "+" : "-"}`);
+          fileBody += curAnswers.join(',');
+          fileBody += '\n';
+        });
+        return fileBody;
+      }
 
-          })
+      let androidFileBody = '';
+      let androidDevs = users.filter(user => user.stack === 'android');
+      androidFileBody += fillData(androidDevs, androidFileBody);
+      fs.writeFile('./android.csv', ANDROID_FILE_HEADER + androidFileBody, 'utf8', () => {
+        console.log('Результаты по платформе Android выгружены');
+      });
+
+      let iosFileBody = '';
+      let iosDevs = users.filter(user => user.stack === 'iOS');
+      iosFileBody += fillData(iosDevs, iosFileBody);
+      fs.writeFile('./ios.csv', IOS_FILE_HEADER + iosFileBody, 'utf8', () => {
+        console.log('Результаты по платформе iOS выгружены');
+      });
+
+      let globalStatFile = '';
+      globalStatFile
+        += `${users.length.toString()};${androidDevs.length.toString()};${iosDevs.length.toString()};${getSucessPercentString(androidDevs)};${getSucessPercentString(iosDevs)}`;
+
+      fs.writeFile('./global.csv', GLOBAL_STAT_HEADER + globalStatFile, 'utf8', () => {
+        console.log('Глобальная статистика выгружена');
+      });
+
+      let categorySuccessResults = {};
+      let categoryFailedResults = {};
+      categories.forEach(category => {
+        categorySuccessResults[category.title] = 0;
+        categoryFailedResults[category.title] = 0;
+      });
+      users.map(user => {
+        let usersAnswers = R.concat([user.answers], user.oldAnswers);
+        usersAnswers.forEach(attempt => {
+          if (attempt.length > 0)
+            attempt.forEach(answer => {
+              if (answer.isCorrect) {
+                categorySuccessResults[answer.category] += 1;
+              } else {
+                categoryFailedResults[answer.category] += 1;
+              }
+            });
         })
-        .then(_ => mongoose.disconnect())
-    });
-  })
-}
+      });
+      let catStatFileBody = '';
+      categories.map(cat => cat.title)
+        .forEach(key => {
+          let num = Math.floor(
+            (categorySuccessResults[key] * 100) / (categorySuccessResults[key] + categoryFailedResults[key])
+          ).toString();
+          let a = `${key};${num}%\n`;
+          catStatFileBody +=a;
+        });
+      fs.writeFile('./categories.csv', CATEGORIES_STAT_HEADER + catStatFileBody, 'utf8', () => {
+        console.log('Результаты по категориям вопросов выгружены');
+      });
+    })
+    .then(_ => mongoose.disconnect())
+});
+
